35d1b7a6005b5c92e00c34d2b6fb500580c64d73
RudsonPaes <145233212+RudsonPaes@users.noreply.github.com>
2025-05-23 08:33:31 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+	
+	private Integer sourceIndex;
+	private Integer destionIndex;
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestionIndex() {
+		return destionIndex;
+	}
+	public void setDestionIndex(Integer destionIndex) {
+		this.destionIndex = destionIndex;
+	}
+	
+	
+	
+
+}
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,22 +5,49 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
+
+    private final WebMvcConfigurer corsConfigurer;
 	
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository	gameRepository;
+
+    GameListService(WebMvcConfigurer corsConfigurer) {
+        this.corsConfigurer = corsConfigurer;
+    }
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
+	
+		@Transactional
+		public void move(Long listId, int  sourceIbdex, int destinationIndex) {
+		
+			List<GameMinProjection> list = gameRepository.searchByList(listId);
+			
+			GameMinProjection obj = list.remove(sourceIbdex);
+			list.add(destinationIndex, obj);
+			
+		int min = 	sourceIbdex < destinationIndex ? sourceIbdex : destinationIndex;
+		int max = 	sourceIbdex < destinationIndex ? destinationIndex : sourceIbdex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+	}
 
+}
 }
